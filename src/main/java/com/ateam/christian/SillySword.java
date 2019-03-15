ee1e99d6f03e275c7b3a57221493fdc7f2573f01
gaurav_u_gupta <gaurav.u.gupta@oracle.com>
2019-03-15 06:14:25 +0530

    changed 900 to 905

--- src/main/java/com/ateam/christian/SillySword.java
+++ src/main/java/com/ateam/christian/SillySword.java
@@ -3,7 +3,7 @@ package com.ateam.christian;
 import net.minecraft.item.*;
 
 public class SillySword extends ItemSword {
-    private static final int ATTACK_DAMAGE = 900;
+    private static final int ATTACK_DAMAGE = 905;
     private static final float ATTACK_SPEED = 10f;
     public SillySword() {
         super(ItemTier.IRON, ATTACK_DAMAGE, ATTACK_SPEED, (new Item.Properties()).group(ItemGroup.COMBAT));
